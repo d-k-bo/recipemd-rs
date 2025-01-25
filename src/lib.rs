aed65632d218059497b5df7cdfada8f510d13782
d-k-bo <47948262+d-k-bo@users.noreply.github.com>
2025-01-25 20:21:27 +0100

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,6 @@
 //! A library for parsing recipes written in markdown that follows the
 //! [RecipeMD](https://recipemd.org/) specification.
 //!
-//! This implementation tries to be as close as possible to the specification.
-//! This results in some differences to the reference implementation written in Python.
-//! See the [related issue](https://github.com/tstehr/RecipeMD/issues/52) for details.
-//!
 //! # Example
 //!
 //! ```
